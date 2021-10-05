6e8c3cd1bbe1d84b4ca47c9e6f5f908ae70a2370
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-05 23:56:30 +0300

    improved menu

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc,char **argv){
     box(gameWin,0,0);
     mvwprintw(gameWin,1,1,"game");
     wrefresh(gameWin);
-    box(menuWin,0,0);
+    //box(menuWin,0,0);
     mvwprintw(menuWin,1,1,"MENU");
     wrefresh(menuWin);
     keypad(menuWin,true);
@@ -47,16 +47,21 @@ int main(int argc,char **argv){
         {
             case KEY_UP:
                 menuHighlight--;
+                if(menuHighlight<0) menuHighlight = 0;
                 break;
             case KEY_DOWN:
                 menuHighlight++;
+                if(menuHighlight>2) menuHighlight = 2;
                 break;
             default:
                 break;
         }         
+        if(menuChoice == ENTER) break;
     }
-
-
+    werase(menuWin);
+    if(menu[menuHighlight] == "HELP") mvwprintw(menuWin,1,1,"HELP ME :D");
+    else mvwprintw(menuWin,3,1,"in construction");
+    wrefresh(menuWin);
 
     getch();			/* Wait for user input */
     endwin();			/* End curses mode		  */
