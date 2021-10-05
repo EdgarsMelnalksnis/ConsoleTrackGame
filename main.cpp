283b7de58687199f8ee64143d136ccf46c6cb5dc
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-05 23:33:36 +0300

    added menu

--- main.cpp
+++ main.cpp
@@ -1,30 +1,64 @@
+#include <iostream>
 #include <ncurses.h>
 
-#define GAME_WIN_HEIGHT 10
-#define GAME_WIN_WIDTH 20
+#define GAME_WIN_HEIGHT 30
+#define GAME_WIN_WIDTH 40
 #define GAME_WIN_Y 5
 #define GAME_WIN_X 2
 #define INS_WIN_HEIGHT 10
 #define INS_WIN_WIDTH 20
 #define INS_WIN_Y 5
 #define INS_WIN_X GAME_WIN_X + GAME_WIN_WIDTH + 5
+#define ENTER 10
 
 int main(int argc,char **argv){	
+    WINDOW* menuWin;
+    WINDOW* gameWin;
+    std::string menu[3] = {"GAME","TRACK","HELP"};
+    int menuChoice;
+    int menuHighlight = 0;
+
     initscr();
     cbreak();//ctr+c exits program
-   // int win_height=10,win_width=20,win_y=5,win_x=2;
-	
-    WINDOW *insWin = newwin(INS_WIN_HEIGHT,INS_WIN_WIDTH,INS_WIN_Y,INS_WIN_X);
-    WINDOW *gameWin = newwin(GAME_WIN_HEIGHT,GAME_WIN_WIDTH,GAME_WIN_Y,GAME_WIN_X);
+
+    menuWin = newwin(INS_WIN_HEIGHT,INS_WIN_WIDTH,INS_WIN_Y,INS_WIN_X);   
+    gameWin = newwin(GAME_WIN_HEIGHT,GAME_WIN_WIDTH,GAME_WIN_Y,GAME_WIN_X);   
     refresh();
 
     box(gameWin,0,0);
     mvwprintw(gameWin,1,1,"game");
     wrefresh(gameWin);
-    box(insWin,0,0);
-    mvwprintw(insWin,1,1,"Instructions");
-    wrefresh(insWin);
+    box(menuWin,0,0);
+    mvwprintw(menuWin,1,1,"MENU");
+    wrefresh(menuWin);
+    keypad(menuWin,true);
+
+    while(1)
+    {
+        for(int i = 0; i<3;i++)
+        {
+            if(i == menuHighlight)
+                wattron(menuWin,A_REVERSE);
+            mvwprintw(menuWin,i+3,1,menu[i].c_str());
+            wattroff(menuWin, A_REVERSE);
+        }
+        menuChoice = wgetch(menuWin);
+        switch(menuChoice)
+        {
+            case KEY_UP:
+                menuHighlight--;
+                break;
+            case KEY_DOWN:
+                menuHighlight++;
+                break;
+            default:
+                break;
+        }         
+    }
+
+
+
     getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-	return 0;
+    endwin();			/* End curses mode		  */
+    return 0;
 }
