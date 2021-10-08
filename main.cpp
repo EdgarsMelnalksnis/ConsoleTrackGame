f05afe301bcf7712a5ac31bed21d58081b0881ec
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-08 17:09:37 +0300

    moved all #defines to config.h

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 #include <ncurses.h>
 #include "vehicle.h"
-
-#define GAME_WIN_HEIGHT 20
-#define GAME_WIN_WIDTH 50
-#define GAME_WIN_Y 5
-#define GAME_WIN_X 10
-#define INS_WIN_HEIGHT 10
-#define INS_WIN_WIDTH 20
-#define INS_WIN_Y 5
-#define INS_WIN_X GAME_WIN_X + GAME_WIN_WIDTH + 5
-#define ENTER 10
+#include "config.h"
 
 int main(int argc,char **argv){	
     //test
