ac77feed07cd16ec634100c3f89062ba8499ba03
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-09 20:00:44 +0300

    added GameObject class

--- main.cpp
+++ main.cpp
@@ -2,23 +2,14 @@
 #include <ncurses.h>
 #include "vehicle.h"
 #include "config.h"
+#include "game_object.h"
+#include "menu.h"
 
-void checkScreenSize(int ymin,int xmin)
-{
-    int y,x;
-    getmaxyx(stdscr,y,x);
-    while(y < ymin || x < xmin)
-    {
-        getmaxyx(stdscr,y,x);
-        clear();
-        printw("Make screen larger. yMax > %d (%d), xMax > %d(%d) !",ymin,y,xmin,x);
-
-        refresh();
-    }
-}
 
 int main(int argc,char **argv){	
     //test
+    GameObject *game= new GameObject();
+
     WINDOW* menuWin;
     std::string menu[3] = {"GAME","TRACK","HELP"};
     int menuChoice;
@@ -30,7 +21,7 @@ int main(int argc,char **argv){
 
     int yMax,xMax;
 
-    checkScreenSize(YMIN,XMIN);
+    game->checkScreenSize(YMIN,XMIN);
 
     clear();
     refresh();
