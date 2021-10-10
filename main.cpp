05b5f34b3cac8b84f8e2a8319fe7ae9f66b3affa
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-10 11:33:06 +0300

    added Menu class constructor

--- main.cpp
+++ main.cpp
@@ -6,80 +6,86 @@
 #include "menu.h"
 
 
+void checkScreenSize(int ymin,int xmin)
+{
+    int y,x;
+    getmaxyx(stdscr,y,x);
+    while(y < ymin || x < xmin)
+    {
+        getmaxyx(stdscr,y,x);
+        clear();
+        printw("Make screen larger. yMax > %d (%d), xMax > %d(%d) !",ymin,y,xmin,x);
+
+        refresh();
+    }
+}
+
+
 int main(int argc,char **argv){	
     //test
-    GameObject *game= new GameObject();
-
-    WINDOW* menuWin;
-    std::string menu[3] = {"GAME","TRACK","HELP"};
-    int menuChoice;
-    int menuHighlight = 0;
+    int yMax,xMax;
+    int startpos[]={1,1};
+    int dim[]={30,40};
 
     initscr();
     noecho();
     cbreak();//ctr+c exits program
-
-    int yMax,xMax;
-
-    game->checkScreenSize(YMIN,XMIN);
+    checkScreenSize(YMIN,XMIN);
 
     clear();
     refresh();
-    getmaxyx(stdscr,yMax,xMax);
+
+    Menu men(startpos,dim);
+
+    refresh();
+    while(1)
+
+        getmaxyx(stdscr,yMax,xMax);
 
     WINDOW* gameWin = newwin(GAME_WIN_HEIGHT,GAME_WIN_WIDTH,(yMax/2)-10,GAME_WIN_X);
     box(gameWin,0,0);
     refresh();
     wrefresh(gameWin);
 
-    Vehicle *c = new Vehicle(gameWin,1,1,'0');
+    //Vehicle *c = new Vehicle(gameWin,1,1,'0');
+    //menuWin = newwin(INS_WIN_HEIGHT,INS_WIN_WIDTH,(yMax/2-10),GAME_WIN_X+GAME_WIN_WIDTH+5);   
+    //  refresh();
+    /*
+       do{
+       c->display();
+       wrefresh(gameWin);
+       }while(c->getMove() != 'x');
 
-
-    menuWin = newwin(INS_WIN_HEIGHT,INS_WIN_WIDTH,(yMax/2-10),GAME_WIN_X+GAME_WIN_WIDTH+5);   
-    refresh();
-
-    box(menuWin,0,0);
-    mvwprintw(menuWin,1,1,"MENU");
-    wrefresh(menuWin);
-    keypad(menuWin,true);
-
-
-
-    do{
-        c->display();
-        wrefresh(gameWin);
-    }while(c->getMove() != 'x');
-
-    while(1)
-    {
-        for(int i = 0; i<3;i++)
-        {
-            if(i == menuHighlight)
-                wattron(menuWin,A_REVERSE);
-            mvwprintw(menuWin,i+3,1,menu[i].c_str());
-            wattroff(menuWin, A_REVERSE);
-        }
-        menuChoice = wgetch(menuWin);
-        switch(menuChoice)
-        {
-            case KEY_UP:
-                menuHighlight--;
-                if(menuHighlight<0) menuHighlight = 0;
-                break;
-            case KEY_DOWN:
-                menuHighlight++;
-                if(menuHighlight>2) menuHighlight = 2;
-                break;
-            default:
-                break;
-        }         
-        if(menuChoice == ENTER) break;
-    }
-    werase(menuWin);
-    if(menu[menuHighlight] == "HELP") mvwprintw(menuWin,1,1,"HELP ME :D");
-    else mvwprintw(menuWin,3,1,"in construction");
-    wrefresh(menuWin);
-
+       while(1)
+       {
+       for(int i = 0; i<3;i++)
+       {
+       if(i == menuHighlight)
+       wattron(menuWin,A_REVERSE);
+       mvwprintw(menuWin,i+3,1,menu[i].c_str());
+       wattroff(menuWin, A_REVERSE);
+       }
+       menuChoice = wgetch(menuWin);
+       switch(menuChoice)
+       {
+       case KEY_UP:
+       menuHighlight--;
+       if(menuHighlight<0) menuHighlight = 0;
+       break;
+       case KEY_DOWN:
+       menuHighlight++;
+       if(menuHighlight>2) menuHighlight = 2;
+       break;
+       default:
+       break;
+       }         
+       if(menuChoice == ENTER) break;
+       }
+       werase(menuWin);
+       if(menu[menuHighlight] == "HELP") mvwprintw(menuWin,1,1,"HELP ME :D");
+       else mvwprintw(menuWin,3,1,"in construction");
+       wrefresh(menuWin);
+     */
     getch();			/* Wait for user input */
     endwin();			/* End curses mode		  */
     return 0;
--- menu.h
+++ menu.h
@@ -4,19 +4,39 @@
 #include <curses.h>
 #include <string>
 #include "menu_item.h"
+#include "config.h"
 
 class Menu {
     public:
-        Menu(WINDOW *win,MenuItem* mi,int num)
-        {
-            this->menuWindow = win;
-            this->mItem = mi;
-            this->numMenus = num;
-        }
-        WINDOW *menuWindow;
-        MenuItem *mItem;    
-        int numMenus;
 
+        Menu(int menuStartPos[],int menuSize[])
+        {
+            this->yxStart[0] = menuStartPos[0];
+            this->yxStart[1] = menuStartPos[1];
+            this->yxSize[0] = menuSize[0];
+            this->yxSize[1] = menuSize[1];
+
+           
+
+            WINDOW *menuWin = newwin(yxSize[0],yxSize[1],(yxSize[0]/2-10),GAME_WIN_X+GAME_WIN_WIDTH+5);
+            box(menuWin,0,0);
+            mvwprintw(menuWin,1,1,"MENU");
+            wrefresh(menuWin);
+            keypad(menuWin,true);
+
+        }
+
+
+        // WINDOW *menuWindow;
+        MenuItem *mItem;//multiple menu items    
+        int *yxStart;
+        int *yxSize;
+        int numMenus;
+        //std::string menu[3] = {"GAME","TRACK","HELP"};
+        int menuChoice;
+        int menuHighlight = 0;
+
+        void drawMenu(int xyStart[],int xySize[]);//methode to draw menu. Class Game will choose where to draw it based on screen size
 };
 
 
