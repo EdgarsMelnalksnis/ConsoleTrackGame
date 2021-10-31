c8331e23883f5743a535620331e5c5e251804014
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-10-31 09:24:52 +0200

    added files with upload

---
+++ helper_functions.h
@@ -0,0 +1,14 @@
+#ifndef _HELPER_FUNCTIONS_H
+#define _HELPER_FUNCTIONS_H
+#include <iostream>
+#include <ncurses.h>
+#include "menu.h"
+
+
+
+void checkScreenSize(int ymin,int xmin);
+void initScreen(void);
+
+
+#endif
+
