3cd56bf75d1953aa87f9483a32c2ecc43fd2ed06
EdgarsMelnalksnis <edgars.melnalksnis@gmail.com>
2021-11-07 17:28:32 +0200

    added files with upload

---
+++ game.cpp
@@ -0,0 +1,19 @@
+#include "game.h"
+
+Game::Game(int* gameStartPos,int* gameSize)
+{
+    this->yxStart = gameStartPos;
+    this->yxSize = gameSize;
+    this->gameWin = newwin(yxSize[0],yxSize[1],yxStart[0],yxStart[1]);
+    drawGame(gameWin);
+}
+
+
+
+void Game::drawGame(WINDOW *win)//methode to draw menu. Class Game will choose where to draw it based on screen size
+{
+    box(win,0,0);
+    mvwprintw(win,1,1,"Game");
+    wrefresh(win);
+}
+
---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef _GAME_H
+#define _GAME_H
+
+#include <curses.h>
+#include <string>
+#include "menu_item.h"
+#include "config.h"
+
+class Game {
+    public:
+        Game(int *gameStartPos,int *gameSize);
+        int updateGame(int);
+
+    private:
+        WINDOW *gameWin;
+        int *yxStart;
+        int *yxSize;
+        
+        void drawGame(WINDOW *);
+};
+
+
+#endif
